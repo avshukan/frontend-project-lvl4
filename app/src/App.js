c24d8c20dcbad68441d1e949f7e063944c599322
Alexander Shukan <avshukan@gmail.com>
2022-07-19 17:39:07 +1100

    add bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,7 @@ import {
   // useParams
 } from "react-router-dom";
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AuthProvider from './context/AuthProvider';
 import LoginButton from './components/LoginButton';
@@ -18,6 +19,8 @@ import MainPage from './components/MainPage';
 import Error404Page from './components/Error404Page';
 import queryString from './routes/queryString';
 import routes from './routes/routes';
+import { Button, Container, Navbar, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const App = () => {
 
@@ -37,36 +40,46 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div className="App">
-          <ul>
-            <li>
-              <Link to={queryString.chatPath()}>Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-            <LoginButton />
-            <LogoutButton />
-          </ul>
-        </div>
-        <Routes>
-          <Route path={queryString.loginPath()} element={<LoginPage />} />
-          <Route path="/about" element={
-            <PrivateRoute><div>About text</div></PrivateRoute>
-          } />
-          <Route path="/topics" element={
-            <PrivateRoute><div>Topics text</div></PrivateRoute>
-          } />
-          <Route path={queryString.chatPath()} element={
-            <PrivateRoute><MainPage /></PrivateRoute>
-          } />
-          <Route path={queryString.errorPath()} element={
-            <PrivateRoute><Error404Page /></PrivateRoute>
-          } />
-        </Routes>
+        <Container fluid='md'>
+          <Navbar bg='light' expand='lg'>
+            <Container>
+              <Navbar.Brand href={queryString.chatPath()}>_Speaky_</Navbar.Brand>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <LinkContainer to={queryString.chatPath()}>
+                  <Button>Chat</Button>
+                </LinkContainer>
+                <LinkContainer to='/about'>
+                  <Button>About</Button>
+                </LinkContainer>
+                <LinkContainer to='/topics'>
+                  <Button>Topics</Button>
+                </LinkContainer>
+                <LoginButton />
+                <LogoutButton />
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Row>
+            <Col>
+              <Routes>
+                <Route path={queryString.loginPath()} element={<LoginPage />} />
+                <Route path="/about" element={
+                  <PrivateRoute><div>About text</div></PrivateRoute>
+                } />
+                <Route path="/topics" element={
+                  <PrivateRoute><div>Topics text</div></PrivateRoute>
+                } />
+                <Route path={queryString.chatPath()} element={
+                  <PrivateRoute><MainPage /></PrivateRoute>
+                } />
+                <Route path={queryString.errorPath()} element={
+                  <PrivateRoute><Error404Page /></PrivateRoute>
+                } />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
       </Router>
     </AuthProvider>
   );
