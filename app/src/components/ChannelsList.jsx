4c7e662cc0352d0d1c97441c8cd77f858ea73e13
Alexander Shukan <avshukan@gmail.com>
2022-08-30 10:54:02 +1100

    add channel switch

--- app/src/components/ChannelsList.jsx
+++ app/src/components/ChannelsList.jsx
@@ -1,9 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classnames from 'classnames';
+import { switchChannel } from "../slices/dataSlice";
 
 const ChannelsList = () => {
+    const dispatch = useDispatch();
+
     const { channels, currentChannelId } = useSelector((state) => state.data);
+
     console.log('channelsList', channels, currentChannelId);
+
+    const onSwitch = (id) => (event) => {
+        event.preventDefault();
+        dispatch(switchChannel({ channelId: id }));
+    };
 
     return (
         <ul className="nav flex-column nav-pills nav-fill px-2">
@@ -11,7 +20,7 @@ const ChannelsList = () => {
                 const classname = classnames("w-100 rounded-0 text-start btn", { 'btn-secondary': currentChannelId !== id }, { 'btn-primary': currentChannelId === id });
                 return (
                     <li key={id} className="nav-item w-100">
-                        <button type="button" className={classname}>
+                        <button type="button" className={classname} onClick={onSwitch(id)}>
                             <span className="me-1">#</span>{name}
                         </button>
                     </li>
