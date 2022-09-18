f6b5a5b3f40d8ab0bf1d6ee897382aac4f5ff6b8
sa <avshukan@gmail.com>
2022-09-19 09:58:12 +1100

    fix: langeuage for LanguageButton

--- app/src/components/LanguageButton.jsx
+++ app/src/components/LanguageButton.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 function LanguageButton() {
   const { t, i18n } = useTranslation();
 
+  const getOtherLanguage = useCallback((lng) => (lng === 'ru' ? 'en' : 'ru'), []);
+
   const toggleLanguage = () => {
-    const lng = i18n.language === 'ru' ? 'en' : 'ru';
+    const lng = getOtherLanguage(i18n.language);
     console.log('i18n.language', i18n.language);
     console.log('lng', lng);
     i18n.changeLanguage(lng, (error, tt) => {
@@ -16,7 +18,7 @@ function LanguageButton() {
   };
 
   // return <Button as={Link} onClick={toggleLanguage}>{t(`langs.${i18n.language}`)}</Button>;
-  return <Button onClick={toggleLanguage}>{t(`langs.${i18n.language}`)}</Button>;
+  return <Button onClick={toggleLanguage}>{t(`langs.${getOtherLanguage(i18n.language)}`)}</Button>;
 }
 
 export default LanguageButton;
