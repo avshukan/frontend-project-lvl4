9b831801a0de839fbe68d9c0165a846b098241b6
Alexander Shukan <avshukan@gmail.com>
2022-10-21 12:28:21 +0600

    stupid commit

--- app/src/slices/index.js
+++ app/src/slices/index.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsSlice from './channelsSlice';
-import modalsSlice from './modalsSlice';
 import messagesSlice from './messagesSlice';
+import modalsSlice from './modalsSlice';
 
 const store = configureStore({
   reducer: {
     channels: channelsSlice,
-    modals: modalsSlice,
     messages: messagesSlice,
+    modals: modalsSlice,
   },
 });
 
